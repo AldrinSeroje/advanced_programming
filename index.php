d72d4cb9f51b65a0aa71824ef9349b530e30b7b9
AldrinSeroje <serojealdrin@gmail.com>
2020-11-02 09:43:26 +0800

    assignment #2

--- index.php
+++ index.php
@@ -1,118 +1,60 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <style>
-  .name
-  {
-    float:left;
-    margin-top: 50px;
-    margin-left: 20px;
-    width: 250px;
-    text-align:center;
-  }
+<?php  
 
-  .calc
-  {
-      border-style:solid;
-      border-width: 1px;
-      float: left;
-      margin-left: 150px;
-      width: 200px;
-      text-align: center;
-  }
+class User
+{
+    public $name;
+    public $username;
+    public $email;
 
-  .box1
-  {
-      width:  100px;
-  }
+    function set_name($name){
+        $this->name = $name;
+    }
 
-  .box2
-  {
-      width: 100px;
-  }
+    function get_name(){
+        return $this->name;
+    }
 
-  .gcd
-  {
-    margin-top: 40px;
-  }
+    function set_username($username){
+        $this->username = $username;
+    }
+
+    function get_username(){
+        return $this->username;
+    }
+
+    function set_email($email){
+        $this->email = $email;
+    }
+
+    function get_email(){
+        return $this->email;
+    }
+
+     function addFriend($name){
+        echo $name . ' You Added a friend <br>';
+    }
+
+    function postStatus($name){
+        echo $name .' You Posted a Status <br>';
+    } 
+}
+    $credentials = new User();
+    $credentials->set_name('Aldrin');
+    $credentials->set_username('Drin1');
+    $credentials->set_email('serojealdrin@gmail.com');
+
+    echo 'Name: ' . $credentials->get_name();
+    echo '<br>';
+    echo 'Username: ' . $credentials->get_username();
+    echo '<br>';
+    echo 'Email: ' . $credentials->get_email();
+    echo '<br>';
+    echo '<br>';
+
+    $credentials->addFriend('Aldrin');
+    echo '<br>';
+    $credentials->postStatus('Aldrin');
 
 
 
-
-  </style>
-  <body>
-      <div class="name">
-        <h1>Aldrin Seroje</h1>
-      </div>
-
-      <div class="calc">
-         <h3 class="calcu">Calculator</h3>
-         <form class="" action="index.php" method="post">
-
-        <p> Input first number: </p>
-        <input type="text" name="num1" value="" class="box1">
-
-        <p> Input second number: </p>
-        <input type="text" name="num2" value="" class="box2">
-        <br>
-       <p>Operation</p>
-
-       <input type="submit" name="add" value="+">
-       <input type="submit" name="min" value="-">
-       <input type="submit" name="mul" value="*">
-       <input type="submit" name="div" value="/">
-
-    </form>
-     <div class="gcd">
-     <p><b>Finding GCD</b></p>
-     <form class="" action="index.php" method="post">
-      <p> Input 1st number</p>
-
-      <input type="text" name="num3" value="" class="box2">
-      <p> Input 2nd number</p>
-
-      <input type="text" name="num4" value="" class="box2">
-      <br>
-      <br>
-      <input type="submit" name="submit" value="Submit">
-    </form>
-    </div>
-
-    <?php
-      if (!empty($_POST))
-      { ?>
-
-         <h1 class="answer"><?php echo "Answer ↓"; ?></h1>
-
-        <?php if (isset($_POST['add']))
-        {
-           echo $_POST['num1'] + $_POST['num2'];
-              $result1;
-        }
-        else if (isset($_POST['min']))
-        {
-            echo $_POST['num1'] - $_POST['num2'];
-        }
-        else if (isset($_POST['mul'])) {
-            echo $_POST['num1'] * $_POST['num2'];
-        }
-        else if (isset($_POST['div'])) {
-            echo $_POST['num1'] / $_POST['num2'];
-        }
-        else {
-            $gcd = 1;
-            for ($i=1; $i <= $_POST['num3'] && $i <= $_POST['num4'] ; $i++)
-            {
-              if($_POST['num3'] % $i == 0 && $_POST['num4'] % $i == 0)
-              $gcd = $i;
-          }
-           echo $gcd;
-        }
-      }
-    ?>
-    </input>
-  </body>
-</html>
+?>
